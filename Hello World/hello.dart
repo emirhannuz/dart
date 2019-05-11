0178822a265ff3ae84f011ec53c774276b7d0a8b
Emirhan Uzuner <emirhannuz@gmail.com>
2019-05-11 16:53:01 +0300

    Initial commit

---
+++ Hello World/hello.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  print("hello world!");
+/*
+  print("object " +"object"); // artı(+) operatorüyle iki string birleştirilebilir.
+  print("object " * 5); //çarpı(*) operatörüyle kelimeyi belirlediğiniz sayi kadar tekrar ettirebilirsiniz.
+  
+  //aritmetik işlemler
+  print(2+2);
+  print(2/3);
+  print(2*3);
+  print(2-3);
+  */
+}
